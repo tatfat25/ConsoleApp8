05b166ece0b3091d357f3230fc860192cf35c81f
huca06001 <huca06001@E-PW05C37R>
2024-11-14 12:56:08 +0100

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApp8
+{
+    class Program
+    {
+       static void Main (string[] args)
+        {
+            Console.WriteLine("Har du gått ut ur gymnasiet svara j för ja och n för nej");
+            string Gymnasie = Console.ReadLine();
+            Console.WriteLine("Vad är din ålder");
+            int Ålder = int.Parse(Console.ReadLine());
+            if (Gymnasie == "j" && Ålder <= 22)
+            {
+                Console.WriteLine("Vi vill anställa dig");
+            } 
+            else
+            {
+                Console.WriteLine("Vi vill inte anställa dig");
+            }
+        }
+    }
+}
